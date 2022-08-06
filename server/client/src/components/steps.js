8caf334bd177314b57568b04869baa0bd9980bef
Samuel Zimel <samuel.c.zimel@gmail.com>
2022-08-06 12:29:14 -0500

    video

--- server/client/src/components/steps.js
+++ server/client/src/components/steps.js
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-
+import ReactPlayer from 'react-player'
 
 const Steps = () => {
   const navigate = useNavigate();
@@ -14,16 +14,22 @@ const Steps = () => {
 
   return (
     <div className='row mt-5 pt-5 '>
-      <div className="offset-md-3 col-md-6 color edges">
-        <div className="font pt-4 m-3">
+      <div className="mx-auto col-md-5 color edges p-4 ">
+        <ReactPlayer 
+          width='100%'
+          height='100%'
+          url='video/Showcase.mp4'
+          controls={true}
+        />
+        {/* <div className="font">
           <strong>Step 1:</strong> <u onClick={() => signUp()}>Sign up</u> or <u onClick={() => logIn()}>Login</u>.
-        </div>
-        <div className="font m-3">
+        </div> */}
+        {/* <div className="font m-3">
           <strong>Step 2:</strong> Create a category!
         </div>
         <div className="font m-3 pb-4">
           <strong>Step 3:</strong> Search & organize away!
-        </div>
+        </div> */}
       </div>
     </div>
   );
